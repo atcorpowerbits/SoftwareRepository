408b12756045197330698655ed2d60681a20581c
victora <victora>
2010-07-26 03:42:53 +0000

    Added UT Test Frameworks for BizFacade and BizInfo.
    git-svn-id: https://sfs.atcorau.local/svn/SoftwareRepository/trunk@53 a8a9b14d-6b92-5b4b-9153-4cd90bb22c97

---
+++ PN022BLL/biz/TestBiz/BizInfoTest.cpp
@@ -0,0 +1,110 @@
+﻿
+#include "StdAfx.h"
+using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+using namespace Biz;
+namespace TestBiz {
+    using namespace System;
+    ref class BizInfoTest;
+    
+    
+    /// <summary>
+///This is a test class for BizInfoTest and is intended
+///to contain all BizInfoTest Unit Tests
+///</summary>
+	[TestClass]
+	public ref class BizInfoTest
+	{
+
+	private: Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^  testContextInstance;
+			 /// <summary>
+			 ///Gets or sets the test context which provides
+			 ///information about and functionality for the current test run.
+			 ///</summary>
+	public: property Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^  TestContext
+			{
+				Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^  get()
+				{
+					return testContextInstance;
+				}
+				System::Void set(Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^  value)
+				{
+					testContextInstance = value;
+				}
+			}
+
+#pragma region Additional test attributes
+			// 
+			//You can use the following additional attributes as you write your tests:
+			//
+			//Use ClassInitialize to run code before running the first test in the class
+			//public: [ClassInitialize]
+			//static System::Void MyClassInitialize(TestContext^  testContext)
+			//{
+			//}
+			//
+			//Use ClassCleanup to run code after all tests in a class have run
+			//public: [ClassCleanup]
+			//static System::Void MyClassCleanup()
+			//{
+			//}
+			//
+			//Use TestInitialize to run code before running each test
+			//public: [TestInitialize]
+			//System::Void MyTestInitialize()
+			//{
+			//}
+			//
+			//Use TestCleanup to run code after each test has run
+			//public: [TestCleanup]
+			//System::Void MyTestCleanup()
+			//{
+			//}
+			//
+#pragma endregion
+			/// <summary>
+			///A test for Instance
+			///</summary>
+	public: [TestMethod]
+			void InstanceTest()
+			{
+				BizInfo^  expected; // TODO: Initialize to an appropriate value
+				BizInfo^  actual;
+				expected = BizInfo::Instance();
+				actual = BizInfo::Instance();
+				Assert::AreEqual(expected, actual);
+			}
+			/// <summary>
+			///A test for GetVersion
+			///</summary>
+	public: [TestMethod]
+			void GetVersionTest()
+			{
+				BizInfo^  target = BizFacade::Instance()->GetBizInfo(); // TODO: Initialize to an appropriate value
+				String^ appVersion = L"blankVersion"; // TODO: Initialize to an appropriate value
+				target->GetVersion(&appVersion);
+				Assert::AreEqual(L"Version 0.1 (stub)", appVersion);
+			}
+			/// <summary>
+			///A test for GetCoName
+			///</summary>
+	public: [TestMethod]
+			void GetCoNameTest()
+			{
+				BizInfo^  target = BizFacade::Instance()->GetBizInfo(); // TODO: Initialize to an appropriate value
+				String^ coName = L"blankCoName"; // TODO: Initialize to an appropriate value
+				target->GetCompanyName(&coName);
+				Assert::AreEqual(L"AtCor Medical (stub)", coName);
+			}			/// <summary>
+			///A test for BizInfo Constructor
+			///</summary>
+	public: [TestMethod]
+			void BizInfoConstructorTest()
+			{
+				BizInfo^  target = BizFacade::Instance()->GetBizInfo();
+				Assert::AreNotEqual(nullptr, target);
+			}
+	};
+}
+namespace TestBiz {
+    
+}
