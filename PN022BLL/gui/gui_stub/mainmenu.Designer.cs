994ec1466e878fcd0921d21065e2cc3ca8f42f23
victora <victora>
2010-08-12 13:52:00 +0000

    Added a new gui stub project to test biz.
    git-svn-id: https://sfs.atcorau.local/svn/SoftwareRepository/trunk@73 a8a9b14d-6b92-5b4b-9153-4cd90bb22c97

---
+++ PN022BLL/gui/gui_stub/mainmenu.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace gui
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pwvCommandToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startCaptureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopCaptureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculateReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simulateCaptureDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pwvCommandToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pwvCommandToolStripMenuItem
+            // 
+            this.pwvCommandToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startCaptureToolStripMenuItem,
+            this.stopCaptureToolStripMenuItem,
+            this.simulateCaptureDataToolStripMenuItem,
+            this.calculateReportToolStripMenuItem});
+            this.pwvCommandToolStripMenuItem.Name = "pwvCommandToolStripMenuItem";
+            this.pwvCommandToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.pwvCommandToolStripMenuItem.Text = "PWV";
+            // 
+            // startCaptureToolStripMenuItem
+            // 
+            this.startCaptureToolStripMenuItem.Name = "startCaptureToolStripMenuItem";
+            this.startCaptureToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.startCaptureToolStripMenuItem.Text = "Start Capture";
+            this.startCaptureToolStripMenuItem.Click += new System.EventHandler(this.startCaptureToolStripMenuItem_Click);
+            // 
+            // stopCaptureToolStripMenuItem
+            // 
+            this.stopCaptureToolStripMenuItem.Name = "stopCaptureToolStripMenuItem";
+            this.stopCaptureToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.stopCaptureToolStripMenuItem.Text = "Stop Capture";
+            this.stopCaptureToolStripMenuItem.Click += new System.EventHandler(this.stopCaptureToolStripMenuItem_Click);
+            // 
+            // calculateReportToolStripMenuItem
+            // 
+            this.calculateReportToolStripMenuItem.Name = "calculateReportToolStripMenuItem";
+            this.calculateReportToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.calculateReportToolStripMenuItem.Text = "Calculate Report";
+            this.calculateReportToolStripMenuItem.Click += new System.EventHandler(this.calculateReportToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // simulateCaptureDataToolStripMenuItem
+            // 
+            this.simulateCaptureDataToolStripMenuItem.Name = "simulateCaptureDataToolStripMenuItem";
+            this.simulateCaptureDataToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.simulateCaptureDataToolStripMenuItem.Text = "Simulate Capture Data";
+            this.simulateCaptureDataToolStripMenuItem.Click += new System.EventHandler(this.simulateCaptureDataToolStripMenuItem_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pwvCommandToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startCaptureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopCaptureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculateReportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simulateCaptureDataToolStripMenuItem;
+    }
+}
+
---
+++ PN022BLL/gui/gui_stub/mainmenu.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Biz;
+
+namespace gui
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 ab1 = new AboutBox1();
+            ab1.Show();
+        }
+
+        private void startCaptureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool rc = Biz.BizFacade.Instance().StartCapture();
+
+            if (rc)
+            {
+                MessageBox.Show("Capture started successfully!!!");
+            }
+            else
+            {
+                MessageBox.Show("Capture start failed???");
+            }
+        }
+
+        private void stopCaptureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool rc = Biz.BizFacade.Instance().StopCapture();
+            if (rc)
+            {
+                MessageBox.Show("Capture stopped successfully!!!");
+            }
+            else
+            {
+                MessageBox.Show("Capture stop failed???");
+            }
+        }
+
+        private void calculateReportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool rc = Biz.BizFacade.Instance().CalculateReport();
+            if (rc)
+            {
+                MessageBox.Show("Report calculated successfully!!!");
+            }
+            else
+            {
+                MessageBox.Show("Report calculation failed???");
+            }
+        }
+
+        private void simulateCaptureDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Biz.BizFacade.Instance().SimulateCaptureData();
+        }
+    }
+}
