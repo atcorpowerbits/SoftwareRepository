2b6c348a603800499b5dffff1ab97cbced0f8d86
victora <victora>
2010-08-23 00:54:39 +0000

    Added dal.h
    git-svn-id: https://sfs.atcorau.local/svn/SoftwareRepository/trunk@89 a8a9b14d-6b92-5b4b-9153-4cd90bb22c97

---
+++ PN022BLL/dal/dal_stub/dal.h
@@ -0,0 +1,22 @@
+// dal.h
+
+#pragma once
+
+namespace DataAccess {
+	public ref class DalConstants
+	{
+	public:
+		static const unsigned int DATA_TONOMETER = 1;
+		static const unsigned int DATA_CUFF_PULSE_COMBO = 2;
+		static const unsigned int DATA_TONOMETER_AND_CUFF_PULSE_COMBO = 3;
+		static const unsigned int DATA_CUFF_ABS_PRESSURE = 4;
+		static const unsigned int DATA_CUFF_NIBP = 5;
+		static const unsigned int DATA_CUFF_PULSE = 6;
+
+		static const unsigned int CONFIG_MODULE_TYPE = 1;
+		static const unsigned int CONFIG_MODULE_CAPABILITY = 2;
+		static const unsigned int CONFIG_MODULE_SN = 3;
+		static const unsigned int CONFIG_MAIN_FW_VERSION = 4;
+	};
+}
+
