6b0fdf7ed549ff77900776bf4e35689f717f8894
victora <victora>
2010-10-15 00:08:56 +0000

    Sprint-1 "Scor" solution from Team Mumbai.
    git-svn-id: https://sfs.atcorau.local/svn/SoftwareRepository/trunk/PN022@144 a8a9b14d-6b92-5b4b-9153-4cd90bb22c97

---
+++ Scor/gui/gui/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Scor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+    }
+}
